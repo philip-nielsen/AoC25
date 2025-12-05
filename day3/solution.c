98a0cd736df4decdd4124d532f51a292d6d92f78
Philip Nielsen <philip.nielsen@ericsson.com>
2025-12-05 16:49:32 +0100

    day3 part 1 done

--- day3/solution.c
+++ day3/solution.c
@@ -2,55 +2,80 @@
 #include <stdio.h>
 
 int numberOfNumbers = 0;
+int sizeOfNumber = 0;
 
-void parseData(int* data) {
+void parseData(int** data) {
     char ch;
-    int number = 0;
     int j = 0;
     while ((ch = getchar()) != EOF) {
         if (ch >= '0' && ch <= '9') {
-            number = number * 10 + ch - '0';
+            data[numberOfNumbers][j] =  ch - '0';
             j++;
         } else if(ch == '\n') {
-            data[numberOfNumbers] = number;
+            sizeOfNumber = j;
             j=0;
-            number = 0;
             numberOfNumbers++;
         }
     }
 }
 
-int numberOfDigits(int n) {
-    if (n < 10) return 1;
-    else return 1 + numberOfDigits(n/10);
-}
-
-int day1(int*data) {
+int day1(int **data) {
     int r = 0;
     int n1 = 0;
     int n2 = 0;
-    int digits = 0;
     for (int i = 0; i < numberOfNumbers; i++) {
-        digits = numberOfDigits(data[i]);
-        char str[digits + 2];
-        sprintf(str, "%llu", data[i]);
-        int n1 = str[0] - '0';
-        int n2 = str[1] - '0';
-        for(int j = 0; j < digits-1; j++) {
-            int tmp = str[j] - '0';
-            if(tmp > n1) {
-                n2 = n1;
+        int n1 = data[i][0];
+        int n2 = data[i][1];
+        for(int j = 1; j < sizeOfNumber; j++) {
+            int tmp = data[i][j];
+            if (tmp > n1 && j == sizeOfNumber-1) {
+                n2 = tmp;
+            } else if(tmp > n1) {
+                n2 = 0;
                 n1 = tmp;
             } else if(tmp > n2) {
                 n2 = tmp;
             }
         }
+        // printf("n1 = %d, n2 = %d\n",n1,n2); 
+        r += n1*10 + n2;
     }
+    return r;
+}
 
+int findLowestIndex(int* array) {
+    int index = 0;
+    return index;
+}
+
+unsigned long long day2(int **data) {
+    unsigned long long r = 0;
+    int cnt = 0;
+    int lowestValue = 0;
+    int lowestValueIndex = 0;
+    for (int i = 0; i < numberOfNumbers; i++) {
+        int n1 = data[i][0];
+        int n2 = data[i][1];
+        int* numArray = calloc(sizeOfNumber, sizeof(int));
+        numArray[0] = n1;
+        numArray[1] = n2;
+        for(int j = 2; j < sizeOfNumber; j++) {
+            int tmp = data[i][j];
+        }
+        // printf("n1 = %d, n2 = %d\n",n1,n2); 
+        r += n1*10 + n2;
+    }
     return r;
 }
 
 int main() {
-    int* data = calloc(1000,sizeof(int*));
+    int** data = calloc(200,sizeof(int*));
+    for (int i = 0; i < 200; i++) {
+        data[i] = calloc(200,sizeof(int));
+    }
+
     parseData(data);
+    printf("day1 = %d\n",day1(data));
+    printf("numberOfnumbers = %d\n", numberOfNumbers);
+    printf("numberOfdigits = %d\n", sizeOfNumber);
 }
